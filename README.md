fc691754c567cb3db03a7f2518ba1f9e8c41bf6e
Lukasz Kuligowski <lukasz.kuligowski@wsp.com>
2020-05-08 17:26:41 +0100

    feat: added parcel-bundler configuration and initial scrits feat: installed husky and lint staged for future git hooks implementation feat: added initial react file setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web-intro
-Personal Website
+Personal Website Front
--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {brand_logo} from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={brand_logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
             Welcome to my website Lukasz K.
-        </p>
+        </h1>
       </header>
     </div>
   );
