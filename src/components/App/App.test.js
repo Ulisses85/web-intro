f487de4bcd15e10bef6b085fb4a4cee0b8e5860f
Lukasz Kuligowski <lukasz.kuligowski@wsp.com>
2020-06-05 15:41:19 +0100

    fix: added scss variable refs in App.scss file, removed font imports, added ignoring rules to test files

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-undef */
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
 describe("App", () => {
   const app = shallow(<App />);
-
   it("renders the title", () => {
     expect(app.find("h1").exists()).toBe(true);
   });
